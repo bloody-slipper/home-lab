aceb4795f8bbd2f1c86e1f10c1685a18f5a79a9c
bloody-slipper <anniershova@gmail.com>
2025-10-30 20:50:21 +0100

    remove comments

--- pages/authorization-page.ts
+++ pages/authorization-page.ts
@@ -17,7 +17,3 @@ export class AuthorizationPage {
         await this.getLoginBatton.click()
     }
 }
-
-/*await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click(); */
