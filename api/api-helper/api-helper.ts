4c4579af8f8b8d677eacb561cabd8c9d8323a596
Maria Shevchuk <mashevchukv@ozon.ru>
2026-01-21 00:45:15 +0300

    refactor + add api test

--- api/api-helper/api-helper.ts
+++ api/api-helper/api-helper.ts
@@ -3,7 +3,10 @@ import { APIRequestContext } from "@playwright/test";
 export class ApiHelper {
   constructor(protected request: APIRequestContext) {}
 
-Подумать 
+// Подумать как сделать гет запрос гибким и переиспользуемым:
+// 1) возможность в параметрах метода передовать квери параметры
+// 2) возможность задавать хедеры
+// 3) возможность отправки без апи ключа
   public async getMethod(apiPath: string, apiKey: string | undefined) {
     this.request.get(`${apiPath}?api_key=${apiKey}`);
   }
